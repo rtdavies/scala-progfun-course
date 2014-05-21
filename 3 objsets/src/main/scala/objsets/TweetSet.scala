f3a050e5effe43799f61a3de9bc46df789580271
Rich Davies <rich.davies@code42.com>
2014-05-20 21:13:09 -0500

    Implement tweet filtering application.

--- 3 objsets/src/main/scala/objsets/TweetSet.scala
+++ 3 objsets/src/main/scala/objsets/TweetSet.scala
@@ -235,14 +235,15 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  val allTweets = TweetReader.allTweets
+  lazy val googleTweets: TweetSet = allTweets.filter(tw => google.indexWhere(s => tw.text.contains(s)) > -1)
+  lazy val appleTweets: TweetSet = allTweets.filter(tw => apple.indexWhere(s => tw.text.contains(s)) > -1)
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
 }
 
 object Main extends App {
